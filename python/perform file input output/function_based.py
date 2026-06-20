6e2b3f2af35f5ece4daa90ac13ecf11621486858
io8-karan <io8.karan1@gmail.com>
2026-06-20 17:59:43 +0530

    kbc game

--- python/perform file input output/function_based.py
+++ python/perform file input output/function_based.py
@@ -1,101 +1,83 @@
-# # Create a login/signup system using file handling and functions.
-# print("login/signup system")
-# print("choose=1 for sign up")
-# print("choose=2 for login ")
-# print("choose=3 for exit")
-# def signup():
-#         print("Enter a detail for sign up")
-#         username=input("Enter username=")
-#         password=input("Enter your Password=")
-#         f=open("D:\Karan\python\perform file input output\detail.txt","a")
-#         f.write(username + "," +password +"\n")
-#         f.close()
-# def login():
-#       print("Enter a detail for login")
-#       username_login=input("Enter username=")
-#       password_login=input("Enter your Password=")
-#       f=open("D:\Karan\python\perform file input output\detail.txt","r+")
-#       found=False
-      
-#       for  data in f:
-#             data=data.strip()
-#             stored_username,stored_password=data.split(",/")
-#             if stored_username==username_login and stored_password==password_login:
-#                   found=True
-#       if found==True:
-#         print("login successfully")  
-#       else:
-#            print("details not matched")
-#       f.close()    
-# while True:
-#     choice=int(input("Enter a choice="))
-#     if choice==1:
-#        signup()
-#     elif choice==2:
-#          login()
-#     elif choice==3:
-#          print("|Exit")
-#          break
-#     else:
-#          print("invalid option")
 
+# Create a login/signup system using file handling and functions.
+try:
+ print("login/signup system")
+ print("choose=1 for sign up")
+ print("choose=2 for login ")
+ print("choose=3 for exit")
+except ValueError:
+ 
+# sign up section    
+ def signup():
+      try: # try section 
+        print("Enter a detail for sign up")
+        username=input("Enter username=")
+        password=input("Enter your Password=")
+        f=open("d:/ai journey/python/perform file input output/detail.txt","r+")
+        for data in f.readlines():
+             new=data.strip()
+             new=new.split(",")
+             if username==new[0]:
+              print("user already exist")
+              break
+        else:
+              f.write(username + ',' + password + "\n")
+              print("New user Successfully added")
+             
+#except section 
 
+      except ValueError:
+        print("Value Error")
+      except IndexError:
+        print("Index Error")
+      except FileNotFoundError:
+        print("File not found error")
+      except Exception as e:
+        print("Other error:", e) 
+    
+#login Section
+           
+def login():
+    
+    try: # try section 
+      print("Enter a detail for login")
+      username_login=input("Enter username=")
+      password_login=input("Enter your Password=")
+      found=False
+      f=open("d:/ai journey/python/perform file input output/detail.txt","r")
+      for data in f.readlines():
+           new=data.strip()
+           new=new.split(",")
+           if username_login==new[0] and password_login==new[1]:
+                found=True
+                break
+      if found:
+           print("Login Successfully")
+       
+      else:
+           print("Login Failed")
+      f.close()
+    
+# except section
 
-
-#  Notes saving  app
-print("Notes Saving App")
-def  show_menu():
- print("choose=1 for showing Menu")
- print("choice=2 fro add notes in flies")
- print("choice=3 for read and display notes")
- print("choice=4 for updating notes")
- print("choice=5 for deleting file data")
- print("choose=6 for exit")
-def add():
- notes=input("Enter your Note=")
- f=open("D:\Karan\python\perform file input output\detail.txt","a")
- f.write(notes + "\n")
- f.close()
-def read():
- f=open("D:\Karan\python\perform file input output\detail.txt","r")
- data=f.read()
- print(data)
- f.close()
-def update():
- f=open("D:\Karan\python\perform file input output\detail.txt","r+")
- line_no=input("Enter A line no.=")
- for i in f:
-   if line_no==i:
-    updates_notes=input("Enter updating data=")
-    f.write(updates_notes)
- data=f.read()
- print(data)
- f.close()
-def delete():
- f=open("D:\Karan\python\perform file input output\detail.txt","r")
- delete_notes=input("Enter deleting data=")
- data=f.readlines()
- f.close()
- f=open("D:\Karan\python\perform file input output\detail.txt","w")
- for line in data:  
-  if delete_notes==line.strip():
-   continue
-  f.write(line)
- f.close()
+    except ValueError:
+        print("Value Error")
+    except IndexError:
+        print("Index Error")
+    except FileNotFoundError:
+        print("File not found error")
+    except Exception as e:
+        print("Other error:", e) 
+        
+# choice section
 
 while True:
     choice=int(input("Enter a choice="))
     if choice==1:
-       show_menu()
+         signup()
     elif choice==2:
-         add()
+         login()
     elif choice==3:
-      read()
-    elif choice==4:
-      update()
-    elif choice==5:
-      delete()
-    elif choice==6:
          print("|Exit")
          break
     else:
--- python/perform file input output/levelup.py
+++ python/perform file input output/levelup.py
@@ -109,3 +109,67 @@ else:
 
 print("total occurance=",count)
 f.close()
+
+
+
+
+
+# #  Notes saving  app
+# print("Notes Saving App")
+# def  show_menu():
+#  print("choose=1 for showing Menu")
+#  print("choice=2 fro add notes in flies")
+#  print("choice=3 for read and display notes")
+#  print("choice=4 for updating notes")
+#  print("choice=5 for deleting file data")
+#  print("choose=6 for exit")
+# def add():
+#  notes=input("Enter your Note=")
+#  f=open("D:\Karan\python\perform file input output\detail.txt","a")
+#  f.write(notes + "\n")
+#  f.close()
+# def read():
+#  f=open("D:\Karan\python\perform file input output\detail.txt","r")
+#  data=f.read()
+#  print(data)
+#  f.close()
+# def update():
+#  f=open("D:\Karan\python\perform file input output\detail.txt","r+")
+#  line_no=input("Enter A line no.=")
+#  for i in f:
+#    if line_no==i:
+#     updates_notes=input("Enter updating data=")
+#     f.write(updates_notes)
+#  data=f.read()
+#  print(data)
+#  f.close()
+# def delete():
+#  f=open("D:\Karan\python\perform file input output\detail.txt","r")
+#  delete_notes=input("Enter deleting data=")
+#  data=f.readlines()
+#  f.close()
+#  f=open("D:\Karan\python\perform file input output\detail.txt","w")
+#  for line in data:  
+#   if delete_notes==line.strip():
+#    continue
+#   f.write(line)
+#  f.close()
+
+# while True:
+#     choice=int(input("Enter a choice="))
+#     if choice==1:
+#        show_menu()
+#     elif choice==2:
+#          add()
+#     elif choice==3:
+#       read()
+#     elif choice==4:
+#       update()
+#     elif choice==5:
+#       delete()
+#     elif choice==6:
+#          print("|Exit")
+#          break
+#     else:
+#          print("invalid option")karan,1234
+
